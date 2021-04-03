73a21c445b798382b32946eff69c6e835656b34b
magictomagic <z1172765646@gmail.com>
2021-04-03 14:02:09 -0700

    add node python data trans

--- src/node_python/main.js
+++ src/node_python/main.js
@@ -1,3 +1,8 @@
-const { spawn } = require('child_process');
+const spawn = require("child_process").spawn;
+const pythonProcess = spawn('python',["first.py", "data send to python as a param"]);
+pythonProcess.stdout.on('data', (data) => {
+    // Do something with the data returned from python script
+    console.log(data.toString())
+});
 
-const child = spawn('dir');
+console.log("callback function is pushed to task queue, so this job on execution context stack will print first")
